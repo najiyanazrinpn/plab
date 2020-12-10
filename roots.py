fbae850d8c8e06048ea35d250457ea005b1de494
Najiya Nazrin P N <71110070+najiyanazrinpn@users.noreply.github.com>
2020-12-10 11:47:52 +0530

    Delete roots.py

--- roots.py
+++
@@ -1,8 +0,0 @@
-from math import *
-a = float(input('Enter a: '))
-b = float(input('Enter b: '))
-c = float(input('Enter c: '))
-det = (b*b)-(4*a*c)
-s1 = (-b+sqrt(det))/(2*a)
-s2 = (-b-sqrt(det))/(2*a)
-print("The roots are ",s1,", ",s2)
