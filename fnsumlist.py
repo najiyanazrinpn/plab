8b13d44d145532fbfa10ea426d8951001ac58754
Najiya Nazrin P N <71110070+najiyanazrinpn@users.noreply.github.com>
2021-01-25 20:42:11 +0530

    Delete fnsumlist.py

--- fnsumlist.py
+++
@@ -1,8 +0,0 @@
-def sumlist(n):
-	sum = 0
-	for i in n:
-		sum+=i
-	return sum
-n = (input("Enter the list of numbers: ")).split(" ")
-n = list(map(int,n))
-print("Sum of integers in the list",sumlist(n))
