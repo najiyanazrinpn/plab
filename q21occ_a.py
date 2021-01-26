d2316b527eaf5b5419a6815f59817fddb914b1c8
Najiya Nazrin P N <71110070+najiyanazrinpn@users.noreply.github.com>
2021-01-26 18:46:15 +0530

    Delete q21occ_a.py

--- q21occ_a.py
+++
@@ -1,7 +0,0 @@
-lst = input("Enter a list: ").split(' ')
-c = 0
-for element in lst:
-    c+=element.count('a')
-for element in lst:
-    c+=element.count('A')
-print("No: of occurence of a in the given list is",c)
