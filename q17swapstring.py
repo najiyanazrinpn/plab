d2ccae69e9b974cae041829efd02706b58c8b99f
Najiya Nazrin P N <71110070+najiyanazrinpn@users.noreply.github.com>
2021-01-26 18:45:33 +0530

    Delete q17swapstring.py

--- q17swapstring.py
+++
@@ -1,6 +0,0 @@
-ls = input("Enter the 2 words separated by a space: ").split(" ")
-print("Before swapping ",ls)
-str1 = ls[0]
-str2 = ls[1]
-k = str1[0]
-print("After swapping ",str1.replace(str2[0],str1[0],1),str2.replace(str1[0],k,1))
