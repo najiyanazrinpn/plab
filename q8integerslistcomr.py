0ad3793a63dc3b84523884969b0fe332a6b49b30
Najiya Nazrin P N <71110070+najiyanazrinpn@users.noreply.github.com>
2021-01-30 00:04:46 +0530

    Update q8integerslistcomr.py

--- q8integerslistcomr.py
+++ q8integerslistcomr.py
@@ -1,3 +1,3 @@
-n = int(input("Enter number of elements: "))
-l1 = [int(input("Enter element ")) for i in range(0,n)]
+l1 = input("Enter the list of integers: ").split(" ")
+l1 = list(map(int,l1))
 print("List of positive numbers in ",l1," is ",[i for i in l1 if i>0])
