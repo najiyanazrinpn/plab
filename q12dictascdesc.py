0406ca41e7c6915d33f7d911f3b7f119c8a378b4
Najiya Nazrin P N <71110070+najiyanazrinpn@users.noreply.github.com>
2021-01-30 10:58:32 +0530

    Update q12dictascdesc.py

--- q12dictascdesc.py
+++ q12dictascdesc.py
@@ -1,17 +1,15 @@
 n = int(input("How many elements? "))
 d = {}
-key=''
 for i in range(0,n):
-	value = int(input("Enter value: "))
-	d[i] = value
+	d[i] = int(input("Enter value: "))
 print("Your dictionary is: \n\t",d)
 for i in range(0,n-1):
 	for j in range(i,n):
 		if(d[i]<d[j]):
 			d[i],d[j]=d[j],d[i]
-print("Sorted dictionary in ascending order: \n\t",d)
+print("Sorted dictionary in descending order: \n\t",d)
 for i in range(0,n-1):
 	for j in range(i,n):
 		if(d[i]>d[j]):
 			d[i],d[j]=d[j],d[i]
-print("Sorted dictionary in descending order: \n\t",d)
+print("Sorted dictionary in ascending order: \n\t",d)
